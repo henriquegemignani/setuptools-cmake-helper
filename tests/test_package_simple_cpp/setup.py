ddb1fabbb011630a2d2b5592a64bd6a0894bd2f5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-14 20:50:44 +0300

    Adding test project

---
+++ tests/test_package_simple_cpp/setup.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+
+import setuptools_cmake_helper
+from setuptools import setup
+
+file_dir = Path(__file__).parent.relative_to(Path().absolute())
+
+cpp_code_dir = os.fspath(file_dir.joinpath("c_src"))
+custom_include_paths = [
+    cpp_code_dir,
+]
+
+ext_modules = [
+    setuptools_cmake_helper.CMakeExtension(
+        "test_package._native",
+        [],
+        cmake_options={
+            "dir": cpp_code_dir,
+            "targets": {
+                "native_test": "lib",
+            },
+        },
+        language="c++",
+        extra_compile_args=[],
+        extra_objects=[],
+    )
+]
+
+
+setup(
+    cmdclass={
+        "build_ext": setuptools_cmake_helper.CMakeBuild,
+    },
+    ext_modules=ext_modules,
+)
